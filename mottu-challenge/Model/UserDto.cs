1b4905cb9093a5e005c2a45e377f39abfde9e46c
labsfiap <labsfiap@PA303MICRO24>
2025-05-21 19:41:50 -0300

    Add project files.

---
+++ mottu-challenge/Model/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace mottu_challenge.Model
+{
+    public class UserDto
+    {
+
+        [Key]
+        public int Id { get; set; } 
+
+        [Required(ErrorMessage = "Usuário precisa ser preenchido")]
+        [MaxLength(20, ErrorMessage = "Precisa ter no máximo 20 caracteres")]
+        public String Username { get; set; }
+        [Required(ErrorMessage = "")]
+
+    }
+}
